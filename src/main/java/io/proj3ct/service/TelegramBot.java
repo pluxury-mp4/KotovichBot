4f08bf282396366ed2a988447cd5f88055764d2d
FeelGood-incorp <mblazhin@bk.ru>
2023-01-23 23:28:08 +0300

    Add case "/start"

--- src/main/java/io/proj3ct/service/TelegramBot.java
+++ src/main/java/io/proj3ct/service/TelegramBot.java
@@ -1,5 +1,6 @@
 package io.proj3ct.service;
 
+import io.proj3ct.KotovichBot.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -7,19 +8,42 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config){
+        this.config = config;
+    }
+
 
     @Override
     public String getBotUsername() {
-        return null;
+        return config.getBotName();
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return config.getToken();
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+
+            }
+        }
+
+    }
+
+    private void startCommandReceived(long chatId, String name){
+
+        String answer = "Привет, " + name;
 
     }
 }
