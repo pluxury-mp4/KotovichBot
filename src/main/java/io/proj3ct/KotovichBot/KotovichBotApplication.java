da56c96257512b0221f526b035788b87006e7175
FeelGood-incorp <mblazhin@bk.ru>
2023-01-23 23:07:58 +0300

    Create a project

---
+++ src/main/java/io/proj3ct/KotovichBot/KotovichBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.KotovichBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotovichBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotovichBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/KotovichBot/KotovichBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.KotovichBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotovichBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
